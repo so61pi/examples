1433c84df5ecb54d5fb52ee4a56ff72daf2b02bc
so61pi <so61pi.re@gmail.com>
2015-03-20 23:02:33 +0700

    A simple custom allocator.

---
+++ C++/CustomAllocator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+template <typename T>
+class my_allocator {
+public:
+    using value_type = T;
+
+    T *allocate(std::size_t n) {
+        std::cout << "allocate\n";
+        return static_cast<T *>(::operator new(n * sizeof(T)));
+    }
+
+    void deallocate(T *p, std::size_t n) {
+        std::cout << "deallocate\n";
+        ::operator delete(p);
+    }
+};
+
+
+int main() {
+    std::vector<int, my_allocator<int>> v{ 1, 2, 3, 4 };
+    for (const auto& i : v)
+        std::cout << i << " ";
+    std::cout << "\n";
+}
