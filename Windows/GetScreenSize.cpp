d0587d8923d3eb0d3d554bca4bc3c2aa5c99d4fd
so61pi <so61pi.re@gmail.com>
2015-03-22 14:02:00 +0700

    Get primary monitor size.

---
+++ Windows/GetScreenSize.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main() {
+    auto xScreen = GetSystemMetrics(SM_CXSCREEN);
+    auto yScreen = GetSystemMetrics(SM_CYSCREEN);
+    if (xScreen == 0 || yScreen == 0) {
+        MessageBox(NULL, L"Cannot get screen's size.", L"Message", MB_ICONINFORMATION);
+        return -1;
+    }
+
+    std::cout << "Screen's width is " << xScreen << " pixels\n"
+        << "Screen's height is " << yScreen << " pixels\n";
+}
