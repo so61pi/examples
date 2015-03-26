de7f576459456478c7c09845a4ccc0331325f3c7
so61pi <so61pi.re@gmail.com>
2015-03-26 09:47:43 +0700

    Create GetErrorMessage and GetLastErrorMessage.

--- Windows/FormatLastError.cpp
+++ Windows/FormatLastError.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <string>
 #include <Windows.h>
+
+
+std::wstring GetErrorMessage(DWORD error) {
+    wchar_t *pszErrorMessage = nullptr;
+
+    if (FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr,
+        error,
+        LANG_SYSTEM_DEFAULT,
+        reinterpret_cast<LPWSTR>(&pszErrorMessage),
+        0,
+        nullptr))
+    {
+        std::wstring errorMessage(pszErrorMessage);
+        LocalFree(pszErrorMessage);
+
+        return errorMessage;
+    }
+
+    return L"";
+}
+
+
+std::wstring GetLastErrorMessage() {
+    return GetErrorMessage(GetLastError());
+}
 
 
 int main() {
     SetLastError(ERROR_INVALID_FUNCTION);
+    std::wcout << GetErrorMessage(GetLastError());
 
-    auto error = GetLastError();
-    if (error != ERROR_SUCCESS) {
-        wchar_t szErrorMessage[100]{};
-        FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, error, LANG_SYSTEM_DEFAULT, szErrorMessage, sizeof(szErrorMessage), nullptr);
-        std::wcout << szErrorMessage << "\n";
-    }
+    SetLastError(ERROR_SUCCESS);
+    std::wcout << GetLastErrorMessage();
 }
