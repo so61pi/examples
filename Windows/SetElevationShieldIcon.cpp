773394c29ead63a8d698ea0d831d84b3cd0b6c3c
so61pi <so61pi.re@gmail.com>
2015-03-30 00:29:42 +0700

    Add the elevation icon to the button.

---
+++ Windows/SetElevationShieldIcon.cpp
@@ -0,0 +1,95 @@
+#include <Windows.h>
+#include <CommCtrl.h>
+
+#pragma comment(lib, "Comctl32.lib")
+
+// enable visual style
+#pragma comment(linker, "\"/manifestdependency:type='win32' \
+    name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+    processorArchitecture='*' publicKeyToken='6595b64144ccf1df' \
+    language='*'\"")
+
+
+const WORD IDM_OK = 1;
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+int APIENTRY wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPTSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    const wchar_t szWindowClass[] = L"WindowClass";
+    const wchar_t szWindowTitle[] = L"Title";
+
+    // register class
+    WNDCLASSEX wcex{};
+    wcex.cbSize = sizeof(wcex);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszClassName = szWindowClass;
+    if (!RegisterClassEx(&wcex)) {
+        MessageBox(NULL, L"Cannot register class.", L"Message", MB_ICONINFORMATION);
+        return -1;
+    }
+
+    // create window
+    auto hWnd = CreateWindow(szWindowClass, szWindowTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+    if (hWnd == NULL) {
+        MessageBox(NULL, L"Cannot create window.", L"Message", MB_ICONINFORMATION);
+        return -1;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    // enter message loop
+    MSG msg{};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE: {
+        auto hButton = CreateWindow(
+            L"BUTTON", L"OK",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            10, 10, 100, 50,
+            hWnd,
+            (HMENU)IDM_OK,
+            (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
+            NULL);
+        if (hButton == NULL)
+            MessageBox(hWnd, L"Cannot create button.", L"Message", MB_ICONINFORMATION);
+        else
+            // can use SendMessage with BCM_SETSHIELD instead
+            Button_SetElevationRequiredState(hButton, TRUE);
+
+        break;
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
