137ab9de63131ff6965674efe8a628ea0675bb41
so61pi <so61pi.re@gmail.com>
2015-05-06 12:50:50 +0700

    Add more comments and change #2 to a different kind of failure.

--- C++/SFINAE.cpp
+++ C++/SFINAE.cpp
@@ -5,6 +5,8 @@
 // #1
 template<typename T,
     typename std::enable_if<std::is_integral<T>::value>::type * = nullptr>
+        // create nullptr of void * if T is a integral type
+        // otherwise create nullptr of <nothing> * -> REMOVE this function from overloading set
 void f(const T&) {
     std::cout << "integral type\n";
 }
@@ -12,7 +14,9 @@ void f(const T&) {
 
 // #2
 template<typename T,
-    typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr>
+    typename = std::enable_if<std::is_floating_point<T>::value>::type>
+        // create type of void if T is a floating point type
+        // otherwise create type of <nothing> -> REMOVE this function from overloading set
 void f(const T&) {
     std::cout << "floating point type\n";
 }
