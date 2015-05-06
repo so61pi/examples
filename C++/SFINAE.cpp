76be586c72adb4210f939d3c25e740ae60a2a6c5
so61pi <so61pi.re@gmail.com>
2015-05-06 14:02:39 +0700

    More comments on using std::enable_if.

--- C++/SFINAE.cpp
+++ C++/SFINAE.cpp
@@ -6,7 +6,9 @@
 template<typename T,
     typename std::enable_if<std::is_integral<T>::value>::type * = nullptr>
         // create nullptr of void * if T is a integral type
-        // otherwise create nullptr of <nothing> * -> REMOVE this function from overloading set
+        // otherwise remove this function from overloading set
+        //
+        // use enable_if this way instead of #2 way
 void f(const T&) {
     std::cout << "integral type\n";
 }
@@ -16,7 +18,12 @@ void f(const T&) {
 template<typename T,
     typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
         // create type of void if T is a floating point type
-        // otherwise create type of <nothing> -> REMOVE this function from overloading set
+        // otherwise remove this function from overloading set
+        //
+        // but don't use enable_if this way, because if the user explicitly
+        // provides template arguments like this
+        //     f<void *, whatever-type>(nullptr);
+        // then the enable_if part is ignored!!!
 void f(const T&) {
     std::cout << "floating point type\n";
 }
@@ -26,4 +33,6 @@ int main() {
     f(1); // remove #2 due to SFINAE -> call #1
 
     f(1.0); // remove #1 due to SFINAE -> call #2
+
+    f<void *, int>(nullptr); // horrible thing happens here!!!
 }
