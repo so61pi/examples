3c348a4873a9b30047d7a0fdc330662fa1cc6fde
so61pi <so61pi.re@gmail.com>
2015-05-06 13:42:58 +0700

    Fix missing typename when using dependent type.
    The previous buggy compiled just fine with MSVC 2013.

--- C++/SFINAE.cpp
+++ C++/SFINAE.cpp
@@ -14,7 +14,7 @@ void f(const T&) {
 
 // #2
 template<typename T,
-    typename = std::enable_if<std::is_floating_point<T>::value>::type>
+    typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
         // create type of void if T is a floating point type
         // otherwise create type of <nothing> -> REMOVE this function from overloading set
 void f(const T&) {
