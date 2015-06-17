9c868719bdc60942fad813364f0a786430271454
Ngọc Thi Huỳnh <so61pi.re@gmail.com>
2015-06-17 04:23:02 +0000

    Simple Variadic Template.

---
+++ C++/VariadicTemplate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+class my_class {};
+
+std::ostream& operator<<(std::ostream& os, const my_class&) {
+    os << "my_class";
+    return os;
+}
+
+
+void print(std::ostream& os) {
+    // recursion end here
+}
+
+template<typename FirstType, typename... Args>
+void print(std::ostream& os, FirstType ft, Args... args) {
+    os << "Number of arguments: " << sizeof...(args) << "\n";
+    
+    // print arguments in reverse order
+    print(os, args...);
+    os << ft;
+}
+
+
+int main() {
+    print(std::cout, 1, "2", 3.0, my_class{});
+}
