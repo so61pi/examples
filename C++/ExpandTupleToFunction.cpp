1adad4a6c30853d24928fadd6c9021e14b09163f
Ngọc Thi Huỳnh <so61pi.re@gmail.com>
2015-09-10 14:09:49 +0000

    Expand a tuple to a function.

--- C++/ExpandTupleToFunction.cpp
+++ C++/ExpandTupleToFunction.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <iostream>
 #include <string>
 #include <tuple>
@@ -6,67 +5,31 @@
 #include <utility>
 
 
-namespace detail {
-
-    template<std::size_t Begin, std::size_t End>
-    struct helper {
-        template<typename Function, typename Tuple, typename... Args>
-        decltype(auto) operator()(Function&& f, Tuple&& t, Args&&... args) const {
-            return helper<Begin+1, End>{}(
-                std::forward<Function>(f),
-                std::forward<Tuple>(t),
-                std::forward<Args>(args)...,
-                std::get<Begin>(std::forward<Tuple>(t))
-            );
-        }
-    };
-
-
-    template<std::size_t End>
-    struct helper<End, End> {
-        template<typename Function, typename Tuple, typename... Args>
-        decltype(auto) operator()(Function&& f, Tuple&&, Args&&... args) const {
-            return f(std::forward<Args>(args)...);
-        }
-    };
-
-} // namespace detail
-
-
-template<typename Function, typename Tuple>
-decltype(auto) expand(Function&& f, Tuple&& t) {
-    return detail::helper<0, std::tuple_size<std::remove_reference_t<Tuple>>::value>{}(std::forward<Function>(f), std::forward<Tuple>(t));
+template<typename F, typename Tuple, std::size_t... I>
+auto expand(F&& f, Tuple&& args, std::index_sequence<I...>) -> decltype(auto) {
+    return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(args))...);
 }
 
 
-void display(int a, char b, double c, std::string const& d) {
-    std::cout << "display : " << a << " " << b << " " << c << " " << d << "\n";
+template<typename F, typename Tuple>
+auto expand(F&& f, Tuple&& args) -> decltype(auto) {
+    return expand(std::forward<F>(f), std::forward<Tuple>(args), std::make_index_sequence<std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
 }
 
 
-class test_t {
-public:
-    static auto create() {
-        return test_t{};
-    }
+void foo(int a, double b, std::string const& c) {
+    std::cout << a << ' ' << b << ' ' << c << '\n';
+}
 
-    void display(int a, char b, double c, std::string& d) {
-        std::cout << "test_t::display : " << a << " " << b << " " << c << " " << d << "\n";
-    }
 
-    void display(int a, char b, double c, std::string& d) const {
-        std::cout << "test_t const::display : " << a << " " << b << " " << c << " " << d << "\n";
-    }
-};
+void bar(int a, double b) {
+    std::cout << a << ' ' << b << '\n';
+}
 
 
 int main() {
-    auto t = std::make_tuple(1, '2', 3.0, std::string{ "4" });
-    expand(display, t);
-
-    test_t test{};
-    expand([&test](auto&&... args) { test.display(std::forward<decltype(args)>(args)...); }, t);
-
-    test_t const& ctest = test;
-    expand([&ctest](auto&&... args) { ctest.display(std::forward<decltype(args)>(args)...); }, t);
+    using namespace std::literals;
+    auto tuple = std::make_tuple(1, 2.0, "abc"s);
+    expand(foo, tuple);
+    expand(bar, tuple, std::index_sequence<0, 1>{});
 }
