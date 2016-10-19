34cebf4ee77760f4717cc1f53168d5a0f5f1ff5a
Ngoc Thi Huynh <so61pi.re@gmail.com>
2016-10-19 14:29:20 +0000

    Get function name.

--- Linux/FunctionCallTrace.cpp
+++ Linux/FunctionCallTrace.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
-// need compiler flag -finstrument-functions
+#include <cxxabi.h>
+#include <dlfcn.h>
+
+
+// need compiler flag -finstrument-functions -rdymanic -ldl
 
 extern "C" {
 
@@ -19,7 +24,6 @@ static unsigned level;
 // __cyg_profile_func_enter -> Indent -> __cyg_profile_func_enter -> Indent...
 void __attribute__((__no_instrument_function__)) Indent(unsigned level);
 
-
 void Indent(unsigned level) {
     while (level) {
         printf("| ");
@@ -28,12 +32,31 @@ void Indent(unsigned level) {
 }
 
 
+char* __attribute__((__no_instrument_function__)) AddressToDemangledName(void* address);
+
+char* AddressToDemangledName(void* address) {
+    Dl_info info = {};
+    if (dladdr(address, &info) && info.dli_sname) {
+        int status = 0;
+        return abi::__cxa_demangle(info.dli_sname, 0, 0, &status);
+    }
+    return nullptr;
+}
+
+
 void __cyg_profile_func_enter(void* func, void* caller) {
     // this function can be called before main,
     // and if we call std::cout int this function,
     // it can lead to SIGSEGV
+
+    char* funcName   = AddressToDemangledName(func);
+    char* callerName = AddressToDemangledName(caller);
+
     Indent(level++);
-    printf("enter %p, caller %p\n", func, caller);
+    printf("enter %p [ %s ], caller %p [ %s ]\n", func, funcName ? funcName : "", caller, callerName ? callerName : "");
+
+    free(funcName);
+    free(callerName);
 }
 
 
@@ -55,3 +78,4 @@ void A() {
 int main() {
     A();
 }
+
