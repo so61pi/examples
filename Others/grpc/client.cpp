b14994efb56b8276513fb4461f02439a3e1733a0
Thi Huynh <so61pi.re@gmail.com>
2017-05-31 20:48:23 +0700

    Add gRPC simple example.

---
+++ Others/grpc/client.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <memory>
+
+#include <grpc/grpc.h>
+#include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+
+#include "rpc.grpc.pb.h"
+
+
+// g++ -o client -std=c++11 client.cpp rpc.pb.cc rpc.grpc.pb.cc -lprotobuf -lgrpc++
+
+
+int main() {
+    auto channel = grpc::CreateChannel("localhost:12345", grpc::InsecureChannelCredentials());
+    auto stub = MyServiceProvider::NewStub(channel);
+
+    grpc::ClientContext context;
+    MyRequest           request;
+    MyResponse          response;
+    request.set_question("???");
+    grpc::Status status = stub->AskMe(&context, request, &response);
+    if (status.ok()) {
+        std::cout << "Answer from server = " << response.answer() << std::endl;
+    } else {
+        std::cerr << "AskMe rpc failed." << std::endl;
+    }
+}
---
+++ Others/grpc/server.cpp
@@ -0,0 +1,47 @@
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc/grpc.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc++/security/server_credentials.h>
+
+#include "rpc.grpc.pb.h"
+
+
+// protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` rpc.proto
+// protoc --cpp_out=. rpc.proto
+// g++ -o server -std=c++11 server.cpp rpc.pb.cc rpc.grpc.pb.cc -lprotobuf -lgrpc++
+
+
+class MyServiceProviderImpl final : public MyServiceProvider::Service {
+public:
+    virtual ::grpc::Status AskMe(::grpc::ServerContext* context, const ::MyRequest* request, ::MyResponse* response) final override {
+        assert(context);
+        assert(request);
+        assert(response);
+
+        response->set_answer("No answer for question " + request->question());
+    }
+};
+
+
+void RunServer(std::string const& address) {
+    MyServiceProviderImpl service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << address << std::endl;
+    server->Wait();
+}
+
+
+int main() {
+    RunServer("0.0.0.0:12345");
+    return 0;
+}
