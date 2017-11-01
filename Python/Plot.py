2b2480d9b8819d8674249c66117c32223e06d4b2
Ngoc Thi Huynh <so61pi.re@gmail.com>
2017-11-01 12:02:56 +0000

    Plot.py created online with Bitbucket

---
+++ Python/Plot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import math
+
+
+if __name__ == '__main__':
+    xAxis = []
+    yAxis = []
+    for i in range(-100000, 100000):
+        x = i * 0.0001
+        y = math.sin(x)
+        xAxis.append(x)
+        yAxis.append(y)
+
+    plt.plot(xAxis, yAxis)
+    plt.show()
