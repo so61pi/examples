ed52453593a528005c2d529f7c3deac344cf6c95
Thi Huynh <so61pi.re@gmail.com>
2018-04-08 16:27:55 +0700

    Update LibSSH2Example.cpp

--- Linux/LibSSH2Example.cpp
+++ Linux/LibSSH2Example.cpp
@@ -20,25 +20,21 @@
 #include <libssh2.h>
 
 
+using namespace std::literals;
+
+
 #define AuthTypeUnknown 0
-#define AuthTypePassword 1
-#define AuthTypeKeyboardInteractive 2
-#define AuthTypePublicKey 4
+#define AuthTypePassword (1 << 0)
+#define AuthTypeKeyboardInteractive (1 << 1)
+#define AuthTypePublicKey (1 << 2)
 
 
-int SetNonBlocking(int sfd) {
-    int flags = fcntl(sfd, F_GETFL, 0);
-    if (flags == -1) {
-        return -1;
-    }
-
-    flags |= O_NONBLOCK;
-    if (fcntl(sfd, F_SETFL, flags) == -1) {
-        return -1;
-    }
-
-    return 0;
-}
+char const* const gUsername = "vagrant";
+char const* const gPassword = "vagrant";
+char const* const gPublicKeyFile = "~/.ssh/key.pub";
+char const* const gPrivateKeyFile = "~/.ssh/key";
+char const* const gServerIp = "127.0.0.1";
+int const         gServerPort = 2222;
 
 
 void ShowSessionError(LIBSSH2_SESSION* session, char const* const message) {
@@ -130,12 +126,107 @@ bool ReadCommandAndExec(LIBSSH2_SESSION* session, LIBSSH2_CHANNEL* channel, bool
 }
 
 
-char const* const gUsername = "vagrant";
-char const* const gPassword = "vagrant";
-char const* const gPublicKeyFile = "~/.ssh/key.pub";
-char const* const gPrivateKeyFile = "~/.ssh/key";
-char const* const gServerIp = "127.0.0.1";
-int const         gServerPort = 2222;
+bool UserAuthAgent(LIBSSH2_SESSION* session) {
+    // connect to ssh-agent
+    auto const agent = libssh2_agent_init(session);
+    if (!agent) {
+        ShowError("Failure initializing ssh-agent support.");
+        return false;
+    }
+    BOOST_SCOPE_EXIT_ALL(&) { libssh2_agent_free(agent); };
+
+    if (libssh2_agent_connect(agent)) {
+        ShowError("Failure connecting to ssh-agent");
+        return false;
+    }
+    BOOST_SCOPE_EXIT_ALL(&) { libssh2_agent_disconnect(agent); };
+
+    if (libssh2_agent_list_identities(agent)) {
+        ShowError("Failure requesting identities to ssh-agent");
+        return false;
+    }
+
+    libssh2_agent_publickey* identity = nullptr;
+    libssh2_agent_publickey* prevIdentity = nullptr;
+    while (true) {
+        auto const rc = libssh2_agent_get_identity(agent, &identity, prevIdentity);
+        if (rc == 1) {
+            ShowError("Couldn't continue authentication");
+            return false;
+        }
+
+        if (rc < 0) {
+            ShowError("Failure obtaining identity from ssh-agent support");
+            return false;
+        }
+
+        if (libssh2_agent_userauth(agent, gUsername, identity)) {
+            ShowError("Authentication with username "s + gUsername + " and public key " + identity->comment + " failed!");
+        } else {
+            ShowInfo("Authentication with username "s + gUsername + " and public key " + identity->comment + " succeeded!");
+            break;
+        }
+        prevIdentity = identity;
+    }
+
+    return true;
+}
+
+
+bool UserAuth(LIBSSH2_SESSION* session) {
+    auto const authlist = ::libssh2_userauth_list(session, gUsername, std::strlen(gUsername));
+    if (!authlist) {
+        ShowSessionError(session, "Failed to get auth list.");
+        return false;
+    }
+
+    int authtype = AuthTypeUnknown;
+    if (std::strstr(authlist, "publickey")) {
+        ShowInfo("Can use publickey authentication method.");
+        authtype |= AuthTypePublicKey;
+    }
+    if (std::strstr(authlist, "password")) {
+        ShowInfo("Can use password authentication method.");
+        authtype |= AuthTypePassword;
+    }
+    if (std::strstr(authlist, "keyboard-interactive")) {
+        ShowInfo("Can use keyboard-interactive authentication method.");
+        authtype |= AuthTypeKeyboardInteractive;
+    }
+
+    if (authtype & AuthTypePublicKey) {
+        if (UserAuthAgent(session)) {
+            ShowInfo("Authenticate successfully by using publickey with ssh-agent.");
+            return true;
+        }
+
+        if (::libssh2_userauth_publickey_fromfile(session, gUsername, gPublicKeyFile, gPrivateKeyFile, gPassword) == 0) {
+            ShowInfo("Authenticate successfully by using publickey.");
+            return true;
+        }
+
+        ShowSessionError(session, "Failed to authenticate by public key.");
+    }
+
+    if (authtype & AuthTypePassword) {
+        if (::libssh2_userauth_password(session, gUsername, gPassword) == 0) {
+            ShowInfo("Authenticate successfully by using password.");
+            return true;
+        }
+        ShowSessionError(session, "Failed to authenticate by password.");
+    }
+
+    if (authtype & AuthTypeKeyboardInteractive) {
+        if (::libssh2_userauth_keyboard_interactive(session, gUsername, KbdCallback) == 0) {
+            ShowInfo("Authenticate successfully by using keyboard-interactive.");
+            return true;
+        }
+        ShowSessionError(session, "Failed to authenticate by keyboard.");
+    }
+
+    ShowError("No authenticating method works.");
+    return false;
+}
 
 
 int main() {
@@ -145,35 +236,35 @@ int main() {
     }
     BOOST_SCOPE_EXIT_ALL(&) { ::libssh2_exit(); };
 
-    auto sock = ::socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    auto const libssh2sock = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (libssh2sock < 0) {
         ShowError(errno);
         return EXIT_FAILURE;
     }
-    BOOST_SCOPE_EXIT_ALL(&) { ::close(sock); };
+    BOOST_SCOPE_EXIT_ALL(&) { ::close(libssh2sock); };
 
     sockaddr_in sin = {};
     sin.sin_family = AF_INET;
     sin.sin_port = htons(gServerPort);
     sin.sin_addr.s_addr = inet_addr(gServerIp);
-    if (::connect(sock, reinterpret_cast<sockaddr*>(&sin), sizeof(sin))) {
+    if (::connect(libssh2sock, reinterpret_cast<sockaddr*>(&sin), sizeof(sin))) {
         ShowError(errno);
         return EXIT_FAILURE;
     }
 
-    auto session = ::libssh2_session_init();
+    auto const session = ::libssh2_session_init();
     BOOST_SCOPE_EXIT_ALL(&) {
         ::libssh2_session_disconnect(session, "normal shutdown");
         ::libssh2_session_free(session);
     };
 
-    if (::libssh2_session_handshake(session, sock) < 0) {
+    if (::libssh2_session_handshake(session, libssh2sock) < 0) {
         ShowSessionError(session, "Cannot establish SSH connection.");
         return EXIT_FAILURE;
     }
 
-    int const sha1HashLen = 20;
-    int const md5HashLen = 16;
+    auto const sha1HashLen = 20;
+    auto const md5HashLen = 16;
     auto fingerprint = ::libssh2_hostkey_hash(session, LIBSSH2_HOSTKEY_HASH_SHA1);
     if (!fingerprint) {
         ShowSessionError(session, "Failed to get hostkey hash.");
@@ -184,51 +275,12 @@ int main() {
     }
     std::cout << std::endl;
 
-    auto authlist = ::libssh2_userauth_list(session, gUsername, std::strlen(gUsername));
-    if (!authlist) {
-        ShowSessionError(session, "Failed to get auth list.");
-        return EXIT_FAILURE;
-    }
-
-    int authtype = AuthTypeUnknown;
-    if (std::strstr(authlist, "password")) {
-        ShowInfo("Can use password authentication method.");
-        authtype |= AuthTypePassword;
-    }
-    if (std::strstr(authlist, "keyboard-interactive")) {
-        ShowInfo("Can use keyboard-interactive authentication method.");
-        authtype |= AuthTypeKeyboardInteractive;
-    }
-    if (std::strstr(authlist, "publickey")) {
-        ShowInfo("Can use publickey authentication method.");
-        authtype |= AuthTypePublicKey;
-    }
-
-    if (authtype & AuthTypePassword) {
-        if (::libssh2_userauth_password(session, gUsername, gPassword)) {
-            ShowSessionError(session, "Failed to authenticate by password.");
-            return EXIT_FAILURE;
-        }
-        ShowInfo("Authenticate successfully by using password.");
-    } else if (authtype & AuthTypeKeyboardInteractive) {
-        if (::libssh2_userauth_keyboard_interactive(session, gUsername, KbdCallback)) {
-            ShowSessionError(session, "Failed to authenticate by keyboard.");
-            return EXIT_FAILURE;
-        }
-        ShowInfo("Authenticate successfully by using keyboard-interactive.");
-    } else if (authtype & AuthTypePublicKey) {
-        if (::libssh2_userauth_publickey_fromfile(session, gUsername, gPublicKeyFile, gPrivateKeyFile, gPassword)) {
-            ShowSessionError(session, "Failed to authenticate by public key.");
-            return EXIT_FAILURE;
-        }
-        ShowInfo("Authenticate successfully by using publickey.");
-    } else {
-        ShowError("Unknown authenticating method.");
+    if (!UserAuth(session)) {
         return EXIT_FAILURE;
     }
 
     // open a generic channel
-    auto channel = ::libssh2_channel_open_session(session);
+    auto const channel = ::libssh2_channel_open_session(session);
     if (!channel) {
         ShowSessionError(session, "Failed to open channel.");
         return EXIT_FAILURE;
@@ -244,8 +296,8 @@ int main() {
     ShowInfo("Request a pty successfully.");
 
     // change pty size
-    int const ptyWidth  = 120;
-    int const ptyHeight = 80;
+    auto const ptyWidth  = 120;
+    auto const ptyHeight = 80;
     if (::libssh2_channel_request_pty_size(channel, ptyWidth, ptyHeight)) {
         ShowSessionError(session, "Failed to change pty size.");
         return EXIT_FAILURE;
@@ -259,14 +311,27 @@ int main() {
     }
     ShowInfo("Open a shell successfully.");
 
-    if (SetNonBlocking(sock)) {
-        ShowError(errno);
-        return EXIT_FAILURE;
-    }
+    // libssh2 socket is always non-blocking
+    // this is set up in libssh2_session_handshake
+    //
+    // libssh2_session_handshake
+    // `-session_startup
+    //   `-session_nonblock
+    //     `-fcntl(O_NONBLOCK)
+    //
+    // libssh2_channel/session_set_blocking only changes api_block_mode
+    //
+    // +------------------+----------------------------------+
+    // |  api_block_mode  | libssh2 api behaviour for EAGAIN |
+    // +------------------+----------------------------------+
+    // | 0 (non-blocking) |    return LIBSSH2_ERROR_EAGAIN   |
+    // +------------------+----------------------------------+
+    // |   1 (blocking)   | loop (BLOCK_ADJUST in session.h) |
+    // +------------------+----------------------------------+
     ::libssh2_channel_set_blocking(channel, 0);
 
     constexpr int MAXCONN = 2;
-    auto epfd = epoll_create(MAXCONN);
+    auto const epfd = epoll_create(MAXCONN);
     if (epfd < 0) {
         ShowError(errno);
         return EXIT_FAILURE;
@@ -280,14 +345,13 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    ev.data.fd = sock;
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev) < 0) {
+    ev.data.fd = libssh2sock;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, libssh2sock, &ev) < 0) {
         ShowError(errno);
         return EXIT_FAILURE;
     }
 
-    bool running = true;
-    while (running) {
+    while (true) {
         epoll_event events[MAXCONN] = {};
         auto ready = epoll_wait(epfd, events, MAXCONN, -1);
         if (ready < 0) {
@@ -301,7 +365,7 @@ int main() {
             auto const fd = events[i].data.fd;
 
             if (es & EPOLLIN) {
-                if (fd == sock) {
+                if (fd == libssh2sock) {
                     if (ChannelReadAndDisplay(session, channel) == false) {
                         return EXIT_FAILURE;
                     }
