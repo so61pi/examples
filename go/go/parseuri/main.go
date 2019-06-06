50a1933b6661d892b9e2f1cbeb7dbd59e9d46f5e
Huynh Ngoc Thi <so61pi.re@gmail.com>
2019-06-06 04:45:41 +0000

    Update go

---
+++ go/go/parseuri/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+)
+
+func parse(uris []string) {
+	fmt.Println("--==[ url.Parse ]==--")
+	for i, uri := range uris {
+		u, e := url.Parse(uri)
+		b, _ := json.MarshalIndent(u, "", "    ")
+
+		fmt.Printf("%v. %s( %s ) - (e = %v)\n", i, "url.Parse", uri, e)
+		fmt.Println(string(b))
+		fmt.Println()
+	}
+}
+
+func parseRequestURI(uris []string) {
+	fmt.Println("--==[ url.ParseRequestURI ]==--")
+	for i, uri := range uris {
+		u, e := url.ParseRequestURI(uri)
+		b, _ := json.MarshalIndent(u, "", "    ")
+
+		fmt.Printf("%v. %s( %s ) - (e = %v)\n", i, "url.ParseRequestURI", uri, e)
+		fmt.Println(string(b))
+		fmt.Println()
+	}
+}
+
+func main() {
+	uris := []string{
+		"example.com",         "example.com/v1",
+		"mailto:example.com",  "mailto:example.com/v1",
+		"https://example.com", "https://example.com/v1",
+
+		"user@example.com",         "user@example.com/v1",
+		"mailto:user@example.com",  "mailto:user@example.com/v1",
+		"https://user@example.com", "https://user@example.com/v1",
+	}
+	parse(uris)
+	parseRequestURI(uris)
+}
