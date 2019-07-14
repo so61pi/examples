6fa8cc5a7be142ac983c131995ec58eea40225b3
Thi Huynh <so61pi.re@gmail.com>
2019-07-14 22:33:12 +0700

    Update java

---
+++ java/spring-test/code/src/main/java/com/so61pi/example/DemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.so61pi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebApplication.class, args);
+	}
+
+}
